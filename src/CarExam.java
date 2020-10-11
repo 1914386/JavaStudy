344960e35ba0d0b0d6ac3eb599f4af685fc54c93
KIM MINJI <sml07113@gmail.com>
2020-10-11 23:58:24 +0900

    2020.10.11

---
+++ src/CarExam.java
@@ -0,0 +1,11 @@
+
+public class CarExam {
+
+	public static void main(String[] args) {
+		// Car() : 생성자 - 메모리에 객체(인스턴스)를 만들라는 명령어
+		// c1, c2 = 만들어진 객체를 참조하는 변수
+		Car c1 = new Car();
+		Car c2 = new Car();
+	}
+
+}
