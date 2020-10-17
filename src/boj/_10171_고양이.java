72e0227cdca3181aafa2ed4fb03137d88cb2411d
KIM MINJI <sml07113@gmail.com>
2020-10-17 23:45:35 +0900

    백준-출력(10171, 10172)

---
+++ src/boj/_10171_고양이.java
@@ -0,0 +1,12 @@
+package boj;
+
+public class _10171_°í¾çÀÌ {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
