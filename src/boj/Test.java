b2ce99f37e563bbe4b06b86fb69e9ebfce7f4873
KIM MINJI <sml07113@gmail.com>
2020-11-08 19:03:36 +0900

    백준-함수(1)

---
+++ src/boj/Test.java
@@ -0,0 +1,13 @@
+package boj;
+
+public class Test {
+	
+	long sum(int[] a) {
+		long result = 0;
+		for(int i=0; i<a.length; i++) {
+			result += a[i];
+		}
+		return result;
+	}
+
+}
---
+++ src/boj/_함수.java
@@ -0,0 +1,13 @@
+package boj;
+
+public class _함수 {
+
+	public static void main(String[] args) {
+		Test boj = new Test();
+		
+		// 15596번: 정수 N개의 합
+		int[] ex = {1, 2, 3, 4, 5, 6, 7, 8};
+		System.out.println(boj.sum(ex));
+	}
+
+}
