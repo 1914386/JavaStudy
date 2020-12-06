05fd48e520dc359f016ab588d95ecc3433863f97
KIM MINJI <sml07113@gmail.com>
2020-12-07 00:39:52 +0900

    프로그래머스-Level1

--- src/programmers/L1_짝수와홀수.java
+++ src/programmers/L1_짝수와홀수.java
@@ -1,9 +1,23 @@
 package programmers;
 
+import java.util.Scanner;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12937
+// 정수 num이 짝수일 경우 Even을 반환하고 홀수인 경우 Odd를 반환하는 함수 solution
+
 public class L1_짝수와홀수 {
 
+	public static String solution(int num) {
+		String answer = "";
+		if(num % 2 == 0) answer = "Even";
+		else answer = "Odd";
+		return answer;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println(solution(sc.nextInt()));
+		sc.close();
 
 	}
 
---
+++ src/programmers/L1_평균.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class L1_평균 {
+	
+	public static double solution(int[] arr) {
+		double answer = 0;
+		double sum = 0;
+		
+		for(int i=0; i<arr.length; i++) {
+			sum += arr[i];
+		}
+		answer = sum / arr.length;
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[] one = {1,2,3,4};
+		System.out.println(solution(one));
+	}
+
+}
